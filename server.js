ad11ecf3302769f6d7f8d048c34a37dddc93ad41
Pieter <pieterstokkink@gmail.com>
2017-01-17 17:10:07 +0000

    added form and app.post

---
+++ server.js
@@ -0,0 +1,26 @@
+'use strict';
+
+const express = require('express');
+const multer = require('multer');
+const path = require('path');
+
+let app = express();
+let uploads = multer({ dest: 'uploads/'});
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', function(req, res){
+    res.sendFile(path.join(__dirname, 'public', 'form.html')); 
+});
+
+app.post('/size', uploads.single('uploadedfile'), function(req, res){
+    res.end(JSON.stringify({size: req.file.size})); 
+});
+
+app.get('*', function(req, res){
+    res.redirect('/');
+});
+
+app.listen(process.env.PORT || 3000, function(){
+    console.log('Listening...');
+});
